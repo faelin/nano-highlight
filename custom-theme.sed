MarcusSchwarz/nanorc

Added three new token-types for coloring: PATH, URL and EMAIL

custom-theme.sed
@@ -18,4 +18,7 @@ s|^WARNING:|color brightwhite,red|
 s|^SECTION:|color brightgreen|
 s|^TAG:|color brightmagenta|
 s|^ATTRIBUTE:|color magenta|
+s|^PATH:|color brightblue|
+s|^URL:|color brightblue|
+s|^EMAIL:|color brightblue|
 s|^WHITESPACE:|color ,green|

faelin/nano-highlight

(cherry picked from commit 0a1c9194a933edc8d66f42dc2e5c5aea2e4ea669)

custom-theme.sed
@@ -16,6 +16,9 @@ s|^DELIMITER:|color brightred|
 s|^JUMP:|color brightcyan|
 s|^WARNING:|color brightwhite,red|
 s|^SECTION:|color white|
-s|^TAG:|color brightblue|
+s|^TAG:|color brightmagenta|
 s|^ATTRIBUTE:|color blue|
+s|^PATH:|color brightgreen|
+s|^URL:|color brightgreen|
+s|^EMAIL:|color brightgreen|
 s|^WHITESPACE:|color ,green|