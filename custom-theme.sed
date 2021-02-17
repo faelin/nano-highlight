MarcusSchwarz/nanorc

Not quite good but an okay(ish) solution for case-insensitive matching of all token types, not just KEYWORDs

custom-theme.sed
@@ -1,25 +1,25 @@
-s|^PLAIN:|color white|
-s|^FUNCTION:|color brightgreen|
-s|^STRING:|color brightcyan|
-s|^COMMENT:|color cyan|
-s|^TYPE:|color brightblue|
-s|^BOOLEAN:|color brightwhite|
-s|^NUMBER:|color brightred|
-s|^VARIABLE:|color brightyellow|
-s|^OPERATOR:|color yellow|
-s|^KEYWORD:|color brightwhite|
-s|^~KEYWORD:|icolor brightwhite|
-s|^MACRO:|color brightmagenta|
-s|^REGEXP:|color blue,cyan|
-s|^ESCAPE:|color black,cyan|
-s|^DELIMITER:|color brightred|
-s|^JUMP:|color brightcyan|
-s|^WARNING:|color brightyellow,red|
-s|^SECTION:|color brightgreen|
-s|^TAG:|color brightwhite|
-s|^ATTRIBUTE:|color green|
-s|^ENTITY:|color brightred|
-s|^PATH:|color brightblue|
-s|^URL:|color brightblue|
-s|^EMAIL:|color brightblue|
-s|^WHITESPACE:|color ,green|
+s|^~\([ABCDEFJKMNOPRSTUWV]\)|i\1|
+s|^\(i\?\)PLAIN:|\1color white|
+s|^\(i\?\)FUNCTION:|\1color brightgreen|
+s|^\(i\?\)STRING:|\1color brightcyan|
+s|^\(i\?\)COMMENT:|\1color cyan|
+s|^\(i\?\)TYPE:|\1color brightblue|
+s|^\(i\?\)BOOLEAN:|\1color brightwhite|
+s|^\(i\?\)NUMBER:|\1color brightred|
+s|^\(i\?\)VARIABLE:|\1color brightyellow|
+s|^\(i\?\)OPERATOR:|\1color yellow|
+s|^\(i\?\)KEYWORD:|\1color brightwhite|
+s|^\(i\?\)MACRO:|\1color brightmagenta|
+s|^\(i\?\)REGEXP:|\1color blue,cyan|
+s|^\(i\?\)ESCAPE:|\1color black,cyan|
+s|^\(i\?\)DELIMITER:|\1color brightred|
+s|^\(i\?\)JUMP:|\1color brightcyan|
+s|^\(i\?\)WARNING:|\1color brightyellow,red|
+s|^\(i\?\)SECTION:|\1color brightgreen|
+s|^\(i\?\)TAG:|\1color brightwhite|
+s|^\(i\?\)ATTRIBUTE:|\1color green|
+s|^\(i\?\)ENTITY:|\1color brightred|
+s|^\(i\?\)PATH:|\1color brightblue|
+s|^\(i\?\)URL:|\1color brightblue|
+s|^\(i\?\)EMAIL:|\1color brightblue|
+s|^\(i\?\)WHITESPACE:|\1color ,green|

faelin/nano-highlight

(cherry picked from commit 3476842f98ab2b1f159a605c35e04e007c6de56e)

custom-theme.sed
@@ -1,24 +1,24 @@
-s|^PLAIN:|color white|
-s|^FUNCTION:|color brightblue|
-s|^STRING:|color green|
-s|^COMMENT:|color cyan|
-s|^TYPE:|color blue|
-s|^BOOLEAN:|color brightyellow|
-s|^NUMBER:|color yellow|
-s|^VARIABLE:|color magenta|
-s|^OPERATOR:|color red|
-s|^KEYWORD:|color brightwhite|
-s|^~KEYWORD:|icolor brightwhite|
-s|^MACRO:|color brightgreen|
-s|^REGEXP:|color black,cyan|
-s|^ESCAPE:|color black,cyan|
-s|^DELIMITER:|color brightred|
-s|^JUMP:|color brightcyan|
-s|^WARNING:|color brightwhite,red|
-s|^SECTION:|color white|
-s|^TAG:|color brightmagenta|
-s|^ATTRIBUTE:|color blue|
-s|^PATH:|color brightgreen|
-s|^URL:|color brightgreen|
-s|^EMAIL:|color brightgreen|
-s|^WHITESPACE:|color ,green|
+s|^~\([ABCDEFJKMNOPRSTUWV]\)|i\1|
+s|^\(i\?\)PLAIN:|\1color white|
+s|^\(i\?\)FUNCTION:|\1color brightblue|
+s|^\(i\?\)STRING:|\1color green|
+s|^\(i\?\)COMMENT:|\1color cyan|
+s|^\(i\?\)TYPE:|\1color blue|
+s|^\(i\?\)BOOLEAN:|\1color brightyellow|
+s|^\(i\?\)NUMBER:|\1color yellow|
+s|^\(i\?\)VARIABLE:|\1color magenta|
+s|^\(i\?\)OPERATOR:|\1color red|
+s|^\(i\?\)KEYWORD:|\1color brightwhite|
+s|^\(i\?\)MACRO:|\1color brightgreen|
+s|^\(i\?\)REGEXP:|\1color brightgreen,cyan|
+s|^\(i\?\)ESCAPE:|\1color black,cyan|
+s|^\(i\?\)DELIMITER:|\1color brightred|
+s|^\(i\?\)JUMP:|\1color brightcyan|
+s|^\(i\?\)WARNING:|\1color brightyellow,red|
+s|^\(i\?\)SECTION:|\1color brightgreen|
+s|^\(i\?\)TAG:|\1color brightwhite|
+s|^\(i\?\)ATTRIBUTE:|\1color green|
+s|^\(i\?\)PATH:|\1color brightblue|
+s|^\(i\?\)URL:|\1color brightgreen|
+s|^\(i\?\)EMAIL:|\1color brightgreen|
+s|^\(i\?\)WHITESPACE:|\1color ,green|